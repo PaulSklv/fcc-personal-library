1627c514467fc775f99bf04a46bc76bb39ddd2ba
Glitch (zany-shelf) <none>
2020-01-10 19:24:51 +0000

    🌞☃️ Checkpoint ./routes/api.js:9182067/8650

--- routes/api.js
+++ routes/api.js
@@ -65,6 +65,7 @@ module.exports = function(app) {
     })
 
     .delete(function(req, res) {
+      connection.db("personalLibrary").collection()
       //if successful response will be 'complete delete successful'
     });
 
@@ -114,19 +115,26 @@ module.exports = function(app) {
 
     .delete(function(req, res) {
       var bookid = req.params.id;
-      connection.then(client => {
-        client
-          .db("personalLibrary")
-          .db("books")
-          .findOneAndDelete({ _id: new ObjectId(bookid) })
-          .then(result => {
-            if(result === null) {
-              res.send("No book exists.");
-            } else {
-              res.send("Delete successfull!");
-            }
-          });
-      });
+      connection
+        .then(client => {
+          client
+            .db("personalLibrary")
+            .collection("books")
+            .findOneAndDelete({ _id: new ObjectId(bookid) })
+            .then(result => {
+              if (result === null) {
+                res.send("No book exists.");
+              } else {
+                res.send("Delete successfull!");
+              }
+            })
+            .catch(error => {
+              return console.log("Something went wrong!", error);
+            });
+        })
+        .catch(error => {
+          return console.log("Something went wrong!", error);
+        });
       //if successful response will be 'delete successful'
     });
 };
